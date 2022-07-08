9e4e8565e304964ff31023e02410313c81082fb8
Martin Kukučka <mar.kukucka@gmail.com>
2022-07-08 14:50:54 +0200

    database connection

--- postMicroservice/settings.py
+++ postMicroservice/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'postMicroservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'posts',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
